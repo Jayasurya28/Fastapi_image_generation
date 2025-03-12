3b9650e71628b8d6f284a97f17927a2d3816670c
Jayasurya28 <jayasurya1970@gmail.com>
2025-03-12 19:28:12 +0530

    gapi

--- aa.py
+++ aa.py
@@ -1,10 +1,10 @@
 import google.generativeai as genai
 
 # Configure the API key
-genai.configure(api_key="GEMINI_API_KEY")
+genai.configure(api_key="AIzaSyDHm6WB50DdfBmj9rcXNx0W9i8RjyvEiOY")
 
 # Initialize the model
-model = genai.GenerativeModel('gemini-pro')
+model = genai.GenerativeModel('gemini-1.0-pro')
 
 # Generate text content
 response = model.generate_content("Hello, world!")
